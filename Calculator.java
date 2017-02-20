9df3b9fb476a71c9e47f8e7c12cfd2a5c543a0e3
klaudia521 <klaudia521@users.noreply.github.com>
2017-02-20 21:01:10 +0000

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,22 @@
+
+public class Calculator {
+	Runway runway;
+	Aircraft aircraft;
+	
+	public Runway getRunway(){
+		return runway;
+	}
+	
+	public void setRunway(Runway runway){
+		this.runway = runway;
+	}
+	
+	public Aircraft getAircraft(){
+		return aircraft;
+	}
+	
+	public void setAircraft(Aircraft airacraft){
+		this.aircraft = aircraft;
+	}
+	// i'm about to finish this part
+}
