b9e94ca2dec61fffe2b4c1c9621ff6e7e3f6d937
klaudia521 <klaudia521@users.noreply.github.com>
2017-02-20 20:55:22 +0000

    Create Airport.java

---
+++ Airport.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Airport {
+	ArrayList<Runway> listOfRunways;
+	
+	public ArrayList<Runway> getListOfRunways(){
+		return listOfRunways;
+	}
+	
+	public void setListOfRunways(ArrayList<Runway> listOfRunways){
+		this.listOfRunways = listOfRunways;
+	}
+	
+}
