388e9f3464771ced66a506ef8af4d83a5ec8658a
klaudia521 <klaudia521@users.noreply.github.com>
2017-02-20 20:59:56 +0000

    Create Runway.java

---
+++ Runway.java
@@ -0,0 +1,59 @@
+
+public class Runway {
+	
+	int toda;
+	int tora;
+	int asda;
+	int lda;
+	char location;
+	ObstacleOnRunway obstacleOnRunway;
+	
+	public int getToda(){
+		return toda;
+	}
+	
+	public int getTora(){
+		return tora;
+	}
+	
+	public int getAsda(){
+		return asda;
+	}
+	
+	public int getLda(){
+		return lda;
+	}
+	
+	public ObstacleOnRunway getObstacleOnRunway(){
+		return obstacleOnRunway;
+	}
+	
+	public char getLocation(){
+		return location;
+	}
+	
+	public void setToda(int toda){
+		this.toda = toda;
+	}
+	
+	public void setTora(int tora){
+		this.tora = tora;
+	}
+	
+	public void setAsda(int asda){
+		this.asda = asda;
+	}
+	public void setLda(int lda){
+		this.lda = lda;
+	}
+	
+	public void setLocation(char location){
+		this.location = location;
+	}
+	
+	public void setObstacleOnRunway( ObstacleOnRunway obstacleOnRunway){
+		this.obstacleOnRunway = obstacleOnRunway;
+	}
+	
+	
+}
