aee8a0e27940659ea43c2efb5becbe047b4a6f8c
jake <jake@jake-Surface-Pro-3>
2017-02-21 17:38:09 +0000

    skeleton code for recaculatedDistances

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,41 @@
+import java.util.ArrayList;
+
 /**
  * Created by steff on 15/02/2017.
  */
 public class Calculator {
+	
+	public ArrayList<Integer> calculate(ObstacleOnRunway or,Aircraft a)
+	{
+		ArrayList<Integer> recalculatedDistances = new ArrayList<Integer>();
+		recalculatedDistances.add(calculateToda(or,a));
+		recalculatedDistances.add(calculateTora(or,a));
+		recalculatedDistances.add(calculateAsda(or,a));
+		recalculatedDistances.add(calculateLda(or,a));
+		return recalculatedDistances;
+		
+		
+	}
+	
+	int calculateToda(ObstacleOnRunway or,Aircraft a)
+	{
+		return 1;
+	}
+	
+	int calculateTora(ObstacleOnRunway or,Aircraft a)
+	{
+		return 1;
+	}
+	
+	int calculateAsda(ObstacleOnRunway or,Aircraft a)
+	{
+		return 1;
+	}
+	
+	int calculateLda(ObstacleOnRunway or,Aircraft a)
+	{
+		return 1;
+	}
+	
+	
 }
