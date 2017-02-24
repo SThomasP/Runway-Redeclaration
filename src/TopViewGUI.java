e0a67b4cc7b5f14e800bcb01b6b18f0cbc5bfbda
Steffan P <stp1g15@soton.ac.uk>
2017-02-24 16:56:27 +0000

    Basic Layout of top view sorted.

--- src/TopViewGUI.java
+++ src/TopViewGUI.java
@@ -6,20 +6,23 @@ public class TopViewGUI extends ViewGUI {
     private Rectangle runwayRec;
     private Line2D centreLine;
     private Polygon clearArea;
+    private Line2D[] sideLines;
+    private final static Stroke dashed = new BasicStroke(3, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[]{9}, 0);
+    private final static Stroke outline = new BasicStroke(2, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL);
 
-    public void init(){
-        setBackground(new Color(51,204,51));
+    public void init() {
+        setBackground(new Color(51, 204, 51));
         clearArea = new Polygon();
         runwayRec = new Rectangle();
     }
 
-    public void redrawView(){
+    public void redrawView() {
         clearArea.reset();
         int height = getHeight();
-        int[] y = {(int) (height*0.3),(int) (height*0.2), (int) (height*0.7),(int) (height*0.8)};
+        int[] y = {(int) (height * 0.3), (int) (height * 0.2), (int) (height * 0.7), (int) (height * 0.8)};
         int width = getWidth();
-        int[] x = {0,(int) (width*0.2),(int) (width*0.25),(int) (width*0.75), (int) (width * 0.8) ,width};
-        clearArea.addPoint(x[0],y[0]);
+        int[] x = {0, (int) (width * 0.17), (int) (width * 0.25), (int) (width * 0.75), (int) (width * 0.83), width};
+        clearArea.addPoint(x[0], y[0]);
         clearArea.addPoint(x[1], y[0]);
         clearArea.addPoint(x[2], y[1]);
         clearArea.addPoint(x[3], y[1]);
@@ -31,16 +34,29 @@ public class TopViewGUI extends ViewGUI {
         clearArea.addPoint(x[2], y[3]);
         clearArea.addPoint(x[1], y[2]);
         clearArea.addPoint(x[0], y[2]);
-        runwayRec = new Rectangle((int) (0.1*width),(int)(0.45*height),(int) (0.8*width),(int) (0.11*height));
+        double halfway = height / 2;
+        sideLines = new Line2D[20];
+        for (int i = 0; i < 10; i++) {
+            double ypoint = halfway - 18 + i * 4;
+            double xpoint = (width * 0.12);
+            sideLines[i] = new Line2D.Double(xpoint, ypoint, x[1], ypoint);
+            sideLines[i + 10] = new Line2D.Double(x[4], ypoint, width - xpoint, ypoint);
+        }
+        runwayRec = new Rectangle((int) (0.05 * width), (int) (0.45 * height), (int) (0.9 * width), (int) (0.10 * height));
+        centreLine = new Line2D.Float((float) x[2], (float) (height / 2), (float) x[3], (float) (height / 2));
         repaint();
     }
 
+
     public void paint(Graphics g) {
         super.paint(g);
-        Graphics2D g2d = (Graphics2D) g;
-        g2d.setColor(new Color(141, 44, 159));
-        g2d.fillPolygon(clearArea);
-        g2d.setColor(Color.gray);
-        g2d.fill(runwayRec);
+        fillShape(new Color(141, 44, 159), g, clearArea);
+        outlineShape(Color.black, g, clearArea, outline);
+        fillShape(Color.gray, g, runwayRec);
+        outlineShape(Color.black, g, runwayRec, outline);
+        outlineShape(Color.white, g, centreLine, dashed);
+        for (Line2D line: sideLines){
+            outlineShape(Color.white, g, line, outline);
+        }
     }
 }
