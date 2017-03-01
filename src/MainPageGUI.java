6dc3ccf970adba40783bfcca1a4bce17adb64e9a
cc10g15 <cc10g15@soton.ac.uk>
2017-03-01 17:52:03 +0000

    editing

--- src/MainPageGUI.java
+++ src/MainPageGUI.java
@@ -12,7 +12,6 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JRadioButton;
 import javax.swing.JTabbedPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
