815c2862758fe97859e7f9801395656c6c4572b1
southamptonjake <jal2g15@soton.ac.uk>
2017-03-15 12:51:26 +0000

    BoundryTesting for runway

--- src/model/RunwayTest.java
+++ src/model/RunwayTest.java
@@ -10,6 +10,8 @@ import static org.junit.Assert.assertEquals;
  */
 public class RunwayTest {
 
+	//normal running methods
+	
     @org.junit.Test
     public void landingOver() throws Exception {
         Runway testRunway = new Runway(27, 'R',3962, 3884,3884,3884,0,50);
@@ -46,5 +48,57 @@ public class RunwayTest {
         assertEquals(testRunway.getToda(),3162);
         assertEquals(testRunway.getAsda(),3084);
     }
+    
+    
+    //boundry testing
+    
+    @org.junit.Test
+    public void resaBoundryTowards() throws Exception {
+        Runway testRunway = new Runway(16, 'C',3900, 3902,3902,3595,306,50);
+        for(int i = 1; i <= 4;i++)
+        {
+        	Obstacle testObstacle = new Obstacle(i,10,2000);
+            testRunway.addObstacle(testObstacle);
+            assertEquals(testRunway.getTora(),2006);
+            assertEquals(testRunway.getToda(),2006);
+            assertEquals(testRunway.getAsda(),2006);
+        }
+        
+    }
+    @org.junit.Test
+    public void heighSlopeFactorBoundryTowards() throws Exception {
+        Runway testRunway = new Runway(16, 'C',3900, 3902,3902,3595,306,50);
+        Obstacle testObstacle = new Obstacle(5,10,2000);
+        testRunway.addObstacle(testObstacle);
+        assertEquals(testRunway.getTora(),1996);
+        assertEquals(testRunway.getToda(),1996);
+        assertEquals(testRunway.getAsda(),1996);
+    }
+    
+    @org.junit.Test
+    public void BlastDistanceBoundryAway() throws Exception {
+        Runway testRunway = new Runway(16, 'C',3900, 3902,3902,3595,306,50);
+        for(int i = 1; i <= 4;i++)
+        {
+        	Obstacle testObstacle = new Obstacle(i,10,50);
+            testRunway.addObstacle(testObstacle);
+            assertEquals(testRunway.getLda(),3245);
+ 
+        }
+        
+    }
+    @org.junit.Test
+    public void heighSlopeFactorBoundryAway() throws Exception {
+        Runway testRunway = new Runway(16, 'C',3900, 3902,3902,3595,306,50);
+        Obstacle testObstacle = new Obstacle(5,10,50);
+        testRunway.addObstacle(testObstacle);
+        assertEquals(testRunway.getLda(),3235);
+    }
+    
+    
+    
+    
+    
+    
 
 }
