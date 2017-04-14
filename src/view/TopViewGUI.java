fbf7e6ff7d35cc00e7179686c6f3686a242a38cf
southamptonjake <jal2g15@soton.ac.uk>
2017-04-14 01:18:35 +0100

    rotate working for evetrthing but rectangles

--- src/view/TopViewGUI.java
+++ src/view/TopViewGUI.java
@@ -34,7 +34,7 @@ public class TopViewGUI extends ViewGUI {
         return obstacleRec;
     }
 
-    public double[] rotate(double orientation,double x,double y)
+    public double[] rotateandzoom(double orientation,double x,double y,double zoom)
     {
     	
     	orientation = (Math.PI * 2) - orientation;
@@ -50,11 +50,13 @@ public class TopViewGUI extends ViewGUI {
     @Override
     public void redrawDistances(int toda, int tora, int lda, int asda) {
   
+    	double zoom = 1;
         super.redrawDistances(toda, tora, lda, asda);
         double orientation = Math.PI /4;
         int height = getHeight();
         int width = getWidth();
         int[] x = {0, (int) (width * 0.17), (int) (width * 0.25), (int) (width * 0.75), (int) (width * 0.83), width};
+        //TODO this part is confusing what does it do
         if (obstacleOnRunway){
             if(obstacleRec.getCenterX() > width/2) {
                 toraLine = new Line2D.Float((float) obstacleRec.getX(), (float) (height / 1.75), (float) x[1], (float) (height / 1.75));
@@ -75,46 +77,48 @@ public class TopViewGUI extends ViewGUI {
             }
         }
         else {
-            toraLine = new Line2D.Float((float) rotate(orientation,x[4],(height / 1.75))[0], (float) rotate(orientation,x[4],(height / 1.75))[1], (float) rotate(orientation,x[1],(height / 1.75))[0], (float) rotate(orientation,x[1],(height / 1.75))[1]);
-            todaLine = new Line2D.Float((float) width, (float) (height / 2.4), (float) x[1], (float) (height / 2.4));
-            asdaLine = new Line2D.Float((float) (width*0.95), (float) (height / 1.65), (float) x[1], (float) (height / 1.65));
-            ldaLine = new Line2D.Float((float) x[4], (float) (height / 2.7), (float) x[1] + thresholdDistance, (float) (height / 2.7));
+            toraLine = new Line2D.Float((float) rotateandzoom(orientation,x[4],(height / 1.75),zoom)[0], (float) rotateandzoom(orientation,x[4],(height / 1.75),zoom)[1], (float) rotateandzoom(orientation,x[1],(height / 1.75),zoom)[0], (float) rotateandzoom(orientation,x[1],(height / 1.75),zoom)[1]);
+            todaLine = new Line2D.Float((float) rotateandzoom(orientation,width,(height / 2.4),zoom)[0], (float) rotateandzoom(orientation,width,(height / 2.4),zoom)[1], (float) rotateandzoom(orientation,x[1],(height / 2.4),zoom)[0], (float) rotateandzoom(orientation,x[1],(height / 2.4),zoom)[1]);
+            asdaLine = new Line2D.Float((float) rotateandzoom(orientation,(width*0.95),(height / 1.65),zoom)[0], (float) rotateandzoom(orientation,(width*0.95),(height / 1.65),zoom)[1], (float) rotateandzoom(orientation,x[1],(height / 1.65),zoom)[0], (float) rotateandzoom(orientation,x[1],(height / 1.65),zoom)[1]);
+            ldaLine = new Line2D.Float((float) rotateandzoom(orientation,x[4],(height / 2.7),zoom)[0], (float) rotateandzoom(orientation,x[4],(height / 2.7),zoom)[1], (float) rotateandzoom(orientation,x[1] + thresholdDistance,(height / 2.7),zoom)[0], (float) rotateandzoom(orientation,x[1] + thresholdDistance,(height / 2.7),zoom)[1]);
         }
         
     }
 
     public void redrawView() {
-        double orientation = Math.PI;
+        double orientation = Math.PI /4;
+        double zoom = 1;
         clearArea.reset();
         int height = getHeight();
         int[] y = {(int) (height * 0.3), (int) (height * 0.2), (int) (height * 0.7), (int) (height * 0.8)};
         int width = getWidth();
         int[] x = {0, (int) (width * 0.17), (int) (width * 0.25), (int) (width * 0.75), (int) (width * 0.83), width};
-        clearArea.addPoint(x[0], y[0]);
-        clearArea.addPoint(x[1], y[0]);
-        clearArea.addPoint(x[2], y[1]);
-        clearArea.addPoint(x[3], y[1]);
-        clearArea.addPoint(x[4], y[0]);
-        clearArea.addPoint(x[5], y[0]);
-        clearArea.addPoint(x[5], y[2]);
-        clearArea.addPoint(x[4], y[2]);
-        clearArea.addPoint(x[3], y[3]);
-        clearArea.addPoint(x[2], y[3]);
-        clearArea.addPoint(x[1], y[2]);
-        clearArea.addPoint(x[0], y[2]);
+        clearArea.addPoint((int)rotateandzoom(orientation,x[0],y[0],zoom)[0],(int)rotateandzoom(orientation,x[0],y[0],zoom)[1] );
+        clearArea.addPoint((int)rotateandzoom(orientation,x[1],y[0],zoom)[0],(int)rotateandzoom(orientation,x[1],y[0],zoom)[1] );
+        clearArea.addPoint((int)rotateandzoom(orientation,x[2],y[1],zoom)[0],(int)rotateandzoom(orientation,x[2],y[1],zoom)[1] );
+        clearArea.addPoint((int)rotateandzoom(orientation,x[3],y[1],zoom)[0],(int)rotateandzoom(orientation,x[3],y[1],zoom)[1] );
+        clearArea.addPoint((int)rotateandzoom(orientation,x[4],y[0],zoom)[0],(int)rotateandzoom(orientation,x[4],y[0],zoom)[1] );
+        clearArea.addPoint((int)rotateandzoom(orientation,x[5],y[0],zoom)[0],(int)rotateandzoom(orientation,x[5],y[0],zoom)[1] );
+        clearArea.addPoint((int)rotateandzoom(orientation,x[5],y[2],zoom)[0],(int)rotateandzoom(orientation,x[5],y[2],zoom)[1] );
+        clearArea.addPoint((int)rotateandzoom(orientation,x[4],y[2],zoom)[0],(int)rotateandzoom(orientation,x[4],y[2],zoom)[1] );
+        clearArea.addPoint((int)rotateandzoom(orientation,x[3],y[3],zoom)[0],(int)rotateandzoom(orientation,x[3],y[3],zoom)[1] );
+        clearArea.addPoint((int)rotateandzoom(orientation,x[2],y[3],zoom)[0],(int)rotateandzoom(orientation,x[2],y[3],zoom)[1] );
+        clearArea.addPoint((int)rotateandzoom(orientation,x[1],y[2],zoom)[0],(int)rotateandzoom(orientation,x[1],y[2],zoom)[1] );
+        clearArea.addPoint((int)rotateandzoom(orientation,x[0],y[2],zoom)[0],(int)rotateandzoom(orientation,x[0],y[2],zoom)[1] );
+        //TODO rectangles don work
         runwayRec = new Rectangle((int) (0.05 * width), (int) (0.45 * height), (int) (0.9 * width), (int) (0.10 * height));
-        centreLine = new Line2D.Float((float) x[2], (float) (height / 2), (float) x[3], (float) (height / 2));
-        toraLine = new Line2D.Float((float) rotate(orientation,x[4],(height / 1.75))[0], (float) rotate(orientation,x[4],(height / 1.75))[1], (float) rotate(orientation,x[1],(height / 1.75))[0], (float) rotate(orientation,x[1],(height / 1.75))[1]);
-        todaLine = new Line2D.Float((float) width, (float) (height/2.4), (float) x[1], (float) (height/2.4));
-        asdaLine = new Line2D.Float((float) 0.95*width, (float) (height/1.65), (float) x[1], (float) (height/1.65));
-        ldaLine = new Line2D.Float((float) x[4], (float) (height/2.7), (float) x[1], (float) (height/2.7));
+        centreLine = new Line2D.Float((float) rotateandzoom(orientation,x[2],(height / 2),zoom)[0], (float) rotateandzoom(orientation,x[2],(height / 2),zoom)[1], (float) rotateandzoom(orientation,x[3],(height / 2),zoom)[0], (float) rotateandzoom(orientation,x[3],(height / 2),zoom)[1]);
+        toraLine = new Line2D.Float((float) rotateandzoom(orientation,x[4],(height / 1.75),zoom)[0], (float) rotateandzoom(orientation,x[4],(height / 1.75),zoom)[1], (float) rotateandzoom(orientation,x[1],(height / 1.75),zoom)[0], (float) rotateandzoom(orientation,x[1],(height / 1.75),zoom)[1]);
+        todaLine = new Line2D.Float((float) rotateandzoom(orientation,width,(height / 2.4),zoom)[0], (float) rotateandzoom(orientation,width,(height / 2.4),zoom)[1], (float) rotateandzoom(orientation,x[1],(height / 2.4),zoom)[0], (float) rotateandzoom(orientation,x[1],(height / 2.4),zoom)[1]);
+        asdaLine = new Line2D.Float((float) rotateandzoom(orientation,(width*0.95),(height / 1.65),zoom)[0], (float) rotateandzoom(orientation,(width*0.95),(height / 1.65),zoom)[1], (float) rotateandzoom(orientation,x[1],(height / 1.65),zoom)[0], (float) rotateandzoom(orientation,x[1],(height / 1.65),zoom)[1]);
+        ldaLine = new Line2D.Float((float) rotateandzoom(orientation,x[4],(height / 2.7),zoom)[0], (float) rotateandzoom(orientation,x[4],(height / 2.7),zoom)[1], (float) rotateandzoom(orientation,x[1] + thresholdDistance,(height / 2.7),zoom)[0], (float) rotateandzoom(orientation,x[1] + thresholdDistance,(height / 2.7),zoom)[1]);
         sideLines = new Line2D[20];
         double halfway = height / 2;
         for (int i = 0; i < 10; i++) {
             double ypoint = halfway - 18 + i * 4;
             double xpoint = (width * 0.12);
-            sideLines[i] = new Line2D.Double(xpoint, ypoint, x[1], ypoint);
-            sideLines[i + 10] = new Line2D.Double(x[4], ypoint, width - xpoint, ypoint);
+            sideLines[i] = new Line2D.Double(rotateandzoom(orientation,xpoint,ypoint,zoom)[0], rotateandzoom(orientation,xpoint,ypoint,zoom)[1], rotateandzoom(orientation,x[1],ypoint,zoom)[0], rotateandzoom(orientation,x[1],ypoint,zoom)[1]);
+            sideLines[i + 10] = new Line2D.Double(rotateandzoom(orientation,x[4],ypoint,zoom)[0], rotateandzoom(orientation,x[4],ypoint,zoom)[1], rotateandzoom(orientation,width - xpoint,ypoint,zoom)[0], rotateandzoom(orientation,width - xpoint,ypoint,zoom)[1]);
         }
     }
 
@@ -124,6 +128,7 @@ public class TopViewGUI extends ViewGUI {
         obstacleOnRunway = false;
     }
 
+    //TODO Rectangles dont work
     @Override
     public void addObstacle(int width, int length, int height, int dFromT, int dFromCL) {
         obstacleOnRunway = true;
@@ -136,7 +141,6 @@ public class TopViewGUI extends ViewGUI {
     }
 
     private static void drawRunwayName(String name, double rotation, int x, int y, Graphics g){
-        rotation = Math.toRadians(rotation);
         Graphics2D g2d =  (Graphics2D) g.create();
         g2d.rotate(rotation);
         g2d.setColor(Color.white);
@@ -149,6 +153,8 @@ public class TopViewGUI extends ViewGUI {
 
     public void paint(Graphics g) {
         super.paint(g);
+        double orientation = Math.PI /4;
+        double zoom = 1;
         fillShape(new Color(141, 44, 159), g, clearArea);
         outlineShape(Color.black, g, clearArea, outline);
         fillShape(Color.gray, g, runwayRec);
@@ -158,14 +164,14 @@ public class TopViewGUI extends ViewGUI {
             outlineShape(Color.blue, g, toraLine, outline);
             outlineShape(Color.blue, g, todaLine, outline);
             outlineShape(Color.orange, g, asdaLine, outline);
-            g.drawString(toraString, (int) toraLine.getX2(), (int) toraLine.getY1() + 15);
-            g.drawString(todaString, (int) todaLine.getX2(), (int) todaLine.getY1() + 15);
-            g.drawString(asdaString, (int) asdaLine.getX2(), (int) asdaLine.getY1() + 15);
+            g.drawString(toraString, (int) toraLine.getX2(), (int) toraLine.getY2() + 15);
+            g.drawString(todaString, (int) todaLine.getX2(), (int) todaLine.getY2() + 15);
+            g.drawString(asdaString, (int) asdaLine.getX2(), (int) asdaLine.getY2() + 15);
 
         }
         if (action == LANDING) {
             outlineShape(Color.orange, g, ldaLine, outline);
-            g.drawString(ldaString, (int) ldaLine.getX2(), (int) ldaLine.getY1() + 15);
+            g.drawString(ldaString, (int) ldaLine.getX2(), (int) ldaLine.getY2() + 15);
         }
         for (Line2D line: sideLines){
             outlineShape(Color.white, g, line, outline);
@@ -175,7 +181,7 @@ public class TopViewGUI extends ViewGUI {
             
         }
 
-        drawRunwayName(name,90,getWidth()/5, getHeight()/2 - 18, g);
-        drawRunwayName(inverseName, -90, 4 * getWidth()/5, getHeight()/2 + 18, g);
+        drawRunwayName(name,orientation,(int)rotateandzoom(orientation,getWidth()/5,getHeight()/2 - 18,zoom)[0],(int)rotateandzoom(orientation,getWidth()/5,getHeight()/2 - 18,zoom)[1], g);
+        drawRunwayName(inverseName, orientation,(int)rotateandzoom(orientation,4 * getWidth()/5,getHeight()/2 + 18,zoom)[0], (int)rotateandzoom(orientation,4 * getWidth()/5,getHeight()/2 + 18,zoom)[1], g);
     }
 }
